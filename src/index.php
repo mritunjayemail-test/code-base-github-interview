a15feacde3a5bc64a6e011f4109479d55a37d2ca
Mritunjay Kumar <45771794+mritunjayemail@users.noreply.github.com>
2018-12-11 08:41:09 +0000

    DB Update query
    DB Update query

--- src/index.php
+++ src/index.php
@@ -28,6 +28,82 @@
                 $dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL"); 
                 echo "Connected to MySQL using username - $username, password - $password, host - $hostname<br>"; 
                 $selected = mysql_select_db("$dbname",$dbhandle)   or die("Unable to connect to MySQL DB - check the database name and try again.");
+
+                
+                // Create connection
+$conn = new mysqli($hostname, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+                
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+                
+                
+$sql2 = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Mritunjay', 'Kumar', 'mritunjayemail@gmail.com');";
+$sql2 .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Shaurya', 'Kumar', 'shaurya@goodboy.com');";
+$sql2 .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Rakhi', 'Prasad', 'rakhi@television.com')";
+
+if ($conn->multi_query($sql2) === TRUE) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql2 . "<br>" . $conn->error;
+}                
+                
+
+$sql1 = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql1);
+                
+ echo "<table class="table table-hover">"
+ echo "<thead>"
+ echo "<tr>"
+ echo "<th scope="col">#</th>"
+ echo "<th scope="col">First Name</th>"
+ echo "<th scope="col">Last Name</th>"
+ echo "</tr>"
+ echo "</thead>"
+ echo "<tbody>"      
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      //  echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "<tr>"
+    echo "<th scope="row">" . $row["id"]. "</th>"
+    echo "<td>" . $row["firstname"]. "</td>"
+    echo "<td>" . $row["lastname"]. "</td>"
+    echo "</tr>"
+
+        
+    }
+} else {
+    echo "0 results";
+}
+                
+echo "</tbody>"
+echo "</table>"
+
+$conn->close();
+                
+                
+                
+                
                 ?>
             </div>
         </div>
